68c74761b8f66106f18659a989bea8e280fe61fd
xennygrimmato <vstulsyan@gmail.com>
2018-08-18 13:43:14 +0530

    Add point update, range query

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ using namespace std;
 class IterativeSegTree {
 	int n;
 	unsigned long sz;
-	vector<int> arr;
+	vector<int> arr; // redundant copy of first array from which segtree was created
 	vector<int> tree;
 	// TODO: Pass _combiner_ as std::function to the constructor
 	//       This will keep the design generic.
@@ -32,6 +32,10 @@ class IterativeSegTree {
 		return min(x, y);
 	}
 
+	int identity() {
+		return 0x3f3f3f3f; // identity element for min function is INF
+	}
+
 public:
 	explicit IterativeSegTree(int len_a, vector<int> a) {
 		n = len_a;
@@ -42,6 +46,34 @@ public:
 		construct_tree();
 	}
 
+	void update(int idx, int new_val) {
+		idx += n;
+		tree[idx] = new_val;
+		while (idx > 1) {
+			idx /= 2;
+			tree[idx] = combine(tree[2*idx], tree[2*idx + 1]);
+		}
+	}
+
+	// range query on [left, right)
+	int query(int left, int right) {
+		left += n, right += n;
+		int ret = identity();
+		while (left < right) {
+			if (left & 1) {
+				ret = combine(ret, tree[left]);
+				left++;
+			}
+			if(right & 1) {
+				right--; // first exclude right, because our spec is [left, right)
+				ret = combine(ret, tree[right]);
+			}
+			left >>= 1;
+			right >>= 1;
+		}
+		return ret;
+	}
+
 	void print_tree() {
 		for(int i = 1 ; i < tree.size() ; i++) {
 			cout << i << ": " << tree[i] << "\n";
@@ -49,13 +81,36 @@ public:
 	}
 };
 
-int main() {
-	int n; cin >> n;
-	vector<int> a(n);
-	for(int i = 0 ; i < n ; i++) {
-		cin >> a[i];
-	}
+void test1() {
+	/*
+	 * array of size 2^k
+	 * 8
+		 1 5 3 7 3 6 5 7
+	 */
+	int n = 8;
+	vector<int> a = {1, 5, 3, 7, 3, 6, 5, 7};
 	IterativeSegTree *segTree = new IterativeSegTree(n, a);
 	segTree->print_tree();
+	cout << segTree->query(3, 8); // ans should be 3
+	delete segTree;
+}
+
+void test2() {
+	/*
+	 * array of even size != 2^k
+	 * 6
+		 4 3 8 1 6 7
+	 */
+	int n = 6;
+	vector<int> a = {4, 3, 8, 1, 6, 7};
+	IterativeSegTree *segTree = new IterativeSegTree(n, a);
+	segTree->print_tree();
+	cout << segTree->query(0, 5); // ans should be 1
+	delete segTree;
+}
+
+int main() {
+	test1();
+	test2();
 }
 
